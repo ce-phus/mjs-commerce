6f46fb484daaf17473a0a0cef547bf17090b7225
cephus <pnakitaren2@gmail.com>
2024-07-21 17:48:41 -0400

    task:signup post and Logout redirect

--- commerce/urls.py
+++ commerce/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('myaccount/', myaccount, name='myaccount'),
     path('signup/', signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'userprofile/logout.html'), name = 'logout'),
+    path('logout/', logout, name = 'logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name = 'userprofile/password_reset.html'), name = 'password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = 'userprofile/password_reset_done.html'), name = 'password_reset_done'),
     # API
